1427083c5af0199d9717e50313deda459e335225
Hoat Le <hoatlevan@gmail.com>
2013-08-04 10:43:28 +0000

    Initial setup

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/teracy-official-blog/"
+http_images_path = "/teracy-official-blog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/teracy-official-blog/fonts"
+css_dir = "public/teracy-official-blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
